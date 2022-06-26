19e0f155c50994ef66a376989f89f725274b6e89
Billy Lu <billyfwlu@gmail.com>
2022-06-26 09:35:16 -0700

    completed main form connection to backend, able to send user email to server for find query

--- src/components/pages/PostForm.jsx
+++ src/components/pages/PostForm.jsx
@@ -1,38 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
 
-export default function PostForm({ initialForm, handleSubmit }){
+export default function PostForm({ initialForm, handleSubmit }) {
     const [form, setForm] = useState(initialForm)
-    return(
+    const [pageLoaded, setPageLoaded] = useState(false)
+
+    useEffect(() => {
+        const token = localStorage.getItem("jwt")
+        const user = jwtDecode(token)
+        setForm({ ...form, email: user.email })
+        console.log("current user", user)
+    }, [])
+
+    return (
         <>
             <h1>PostForm</h1>
             <form onSubmit={e => handleSubmit(e, form, setForm)}>
                 <label htmlFor="restaurant">restaurant: </label>
                 <input
-                type="text"
-                id='restaurant'
-                value={form.restaurant}
-                onChange={e => setForm({...form, restaurant: e.target.value})}
+                    type="text"
+                    id='restaurant'
+                    value={form.restaurant}
+                    onChange={e => setForm({ ...form, restaurant: e.target.value })}
                 />
                 <label htmlFor="dish">dish: </label>
                 <input
-                type="text"
-                id='dish'
-                value={form.dish}
-                onChange={e => setForm({...form, dish: e.target.value})}
+                    type="text"
+                    id='dish'
+                    value={form.dish}
+                    onChange={e => setForm({ ...form, dish: e.target.value })}
                 />
                 <label htmlFor="rating">rating: </label>
                 <input
-                type="number"
-                id='rating'
-                value={form.rating}
-                onChange={e => setForm({...form, rating: e.target.value})}
+                    type="number"
+                    id='rating'
+                    value={form.rating}
+                    onChange={e => setForm({ ...form, rating: e.target.value })}
                 />
                 <label htmlFor="content">content: </label>
                 <input
-                type="text"
-                id="content"
-                value={form.content}
-                onChange={e => setForm({...form, content: e.target.value})}
+                    type="text"
+                    id="content"
+                    value={form.content}
+                    onChange={e => setForm({ ...form, content: e.target.value })}
                 />
                 <button type="submit">Submit</button>
             </form>

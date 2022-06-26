1c008efe9b0878f34a2e234308d964379cb75415
marzo20 <myjune91@gmail.com>
2022-06-26 15:32:02 -0700

    posts page edited

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -1,21 +1,22 @@
+import { useEffect } from "react"
 
 export default function Posts({ posts }) {
-  
+    const msg = "No posts. Please Create new Post"
+    
     const post = posts.map((post, i) => {
         return (
             <div key={`post_${i}`}>
-                <h1>Restaurant: {post.restaurant}</h1>
-                <h2>Dish: {post.dish}</h2>
-                <h3>Rate: {post.rating}</h3>
-                <h1>Content: {post.content}</h1>
-
+                <h1>Restaurant: {post.dish.restaurant.name ? post.dish.restaurant.name : ''}</h1>
+                <h2>Dish: {post.dish.dishName ? post.dish.dishName : ''}</h2>
+                <h3>Rate: {post.rating ? post.rating : ''}</h3>
             </div>
         )
     })
     return (
         <>
             <h1>Render all posts</h1>
-            <p>{post}</p>
+            {posts.length>0 ? post : msg}
+            {/* {post} */}
         </>
     )
 }

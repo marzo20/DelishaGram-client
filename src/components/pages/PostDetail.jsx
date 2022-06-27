ede2872b629aebcaec9a83040d1fc2380d48af1d
marzo20 <myjune91@gmail.com>
2022-06-27 11:52:35 -0700

    postDetail page working

--- src/components/pages/PostDetail.jsx
+++ src/components/pages/PostDetail.jsx
@@ -1,14 +1,44 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 export default function PostDetail(){
     const {id} = useParams()
-    // const [post, setPost] =
+    const [post, setPost] = useState({
+        dish: {
+            dishName: '',
+            restaurant: {
+                name: ''
+            }
+        },
+        poster: {
+            userName: ''
+        },
+    })
+    useEffect(() => {
+        const fetchData = async () => {
+           try{
+               const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`)
+               console.log('consologing',response.data)
+               
+            //    const newPost = response.data
+               setPost(response.data)
+               console.log('what is in post',post)
+           }catch(err){
+               console.log(err)
+           }
+        }
+        fetchData()
+    },[])
     return(
         <>
             <h1>PostDetail</h1>
+            <h3>Posted by: {post.poster.userName ? post.poster.userName : 'anonymous'}</h3>
+            <h2>Dish: {post.dish.dishName}</h2>
+            <h2>Restaurant: {post.dish.restaurant.name}</h2>
+            <h2>Rate: {post.rating}</h2>
+            <p>{post.content}</p>
         </>
     )
 }

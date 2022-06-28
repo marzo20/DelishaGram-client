163501674f402295e44692f9328b6b473a7de28a
marzo20 <myjune91@gmail.com>
2022-06-28 12:06:13 -0700

    added delete function

--- src/components/pages/PostDetail.jsx
+++ src/components/pages/PostDetail.jsx
@@ -4,9 +4,9 @@ import axios from 'axios'
 import PostForm from './PostForm'
 
 
-export default function PostDetail({ currentUser }){
+export default function PostDetail({ currentUser }) {
     const navigate = useNavigate()
-    const {id} = useParams()
+    const { id } = useParams()
     const [showForm, setShowForm] = useState(false)
     const [post, setPost] = useState({
         dish: {
@@ -17,59 +17,73 @@ export default function PostDetail({ currentUser }){
         },
         poster: {
             userName: '',
-            _id:'',
+            _id: '',
         },
     })
     useEffect(() => {
         const fetchData = async () => {
-           try{
-               const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`)
-               console.log('consologing',response.data)
-               
-            //    const newPost = response.data
-               setPost(response.data)
-               console.log('what is in post',post)
-           }catch(err){
-               console.log(err)
-           }
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`)
+                console.log('consologing', response.data)
+
+                //    const newPost = response.data
+                setPost(response.data)
+                console.log('what is in post', post)
+            } catch (err) {
+                console.log(err)
+            }
         }
         fetchData()
-    },[])
+    }, [])
     const handleSubmit = async (e, form, setForm) => {
         e.preventDefault()
-        try{
-            const response = await axios.put(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`,form)
+        try {
+            const response = await axios.put(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`, form)
             console.log(response.data)
             setPost(response.data)
             setShowForm(false)
-        }catch(err){
+        } catch (err) {
             console.log('put method failed :(', err)
         }
+    }
+    const handleDelete = (e) => {
+        axios.delete(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/${id}`)
+            .then(response => {
+                navigate('/posts')
+            })
+            .catch(console.warn)
     }
     const renderDetail = (
         <div>
             <h1>PostDetail</h1>
+            <img src={post.image.cloud_id} alt={post.dish.dishName} />
             <h3>Posted by: {post.poster.userName}</h3>
             <h2>Dish: {post.dish.dishName}</h2>
             <h2>Restaurant: {post.dish.restaurant.name}</h2>
             <h2>Rate: {post.rating}</h2>
             <p>{post.content}</p>
-            {post.poster._id === currentUser.id ? <button onClick={()=> setShowForm(!showForm)}>Edit</button>
-             : '' } 
+            {post.poster._id === currentUser.id ? <button onClick={() => setShowForm(!showForm)}>Edit</button>
+                : ''}
+            <div>
+                {post.poster._id === currentUser.id ? <button onClick={() => handleDelete()}>Delete</button>
+                    : ''}
+
+            </div>
         </div>
     )
-    return(
+    return (
         <>
-            {showForm ? 
-            <PostForm 
-            initialForm={{
-                restaurant: post.dish.restaurant.name,
-                dish: post.dish.dishName,
-                rating: post.rating,
-                content: post.content
-            }}
-            handleSubmit={handleSubmit}
-            /> : renderDetail}
+            {showForm ?
+                <PostForm
+                    initialForm={{
+                        restaurant: post.dish.restaurant.name,
+                        dish: post.dish.dishName,
+                        rating: post.rating,
+                        content: post.content
+                    }}
+                    handleSubmit={handleSubmit}
+                /> : renderDetail}
+
         </>
     )
 }

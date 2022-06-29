257d76b4235b571b3d20312c6f1733c56210dc93
Billy Lu <billyfwlu@gmail.com>
2022-06-28 21:12:13 -0700

    added base modal functionality

--- src/components/pages/PostForm.jsx
+++ src/components/pages/PostForm.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import { Navigate , useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
 
-export default function PostForm({ form, setForm, handleSubmit }) {
+export default function PostForm({ form, setForm, handleSubmit, hasModal, openModal, setModalOpen }) {
     let navigate = useNavigate()
-    
+
     const [pageLoaded, setPageLoaded] = useState(false)
     const token = localStorage.getItem("jwt")
     const user = jwtDecode(token)
@@ -15,12 +15,12 @@ export default function PostForm({ form, setForm, handleSubmit }) {
 
     return (
         <>
-            <h1>PostForm</h1>
             <form onSubmit={e => {
                 handleSubmit(e, form, setForm)
                 // navigate(rerouteUrl)
-            }              
+            }
             }>
+                <div>
                 <label htmlFor="restaurant">restaurant: </label>
                 <input
                     type="text"
@@ -29,6 +29,18 @@ export default function PostForm({ form, setForm, handleSubmit }) {
                     onChange={e => setForm({ ...form, restaurant: e.target.value })}
                     required
                 />
+                {hasModal ?
+                    <button
+                        type='button'
+                        onClick={openModal}
+                    >
+                        Search Restaurants
+                    </button>
+                    :
+                    ""
+                }
+                </div>
+                <div>
                 <label htmlFor="dish">dish: </label>
                 <input
                     type="text"
@@ -37,6 +49,8 @@ export default function PostForm({ form, setForm, handleSubmit }) {
                     onChange={e => setForm({ ...form, dish: e.target.value })}
                     required
                 />
+                </div>
+                <div>
                 <label htmlFor="rating">rating: </label>
                 <input
                     type="number"
@@ -45,6 +59,8 @@ export default function PostForm({ form, setForm, handleSubmit }) {
                     onChange={e => setForm({ ...form, rating: e.target.value })}
                     required
                 />
+                </div>
+                <div>
                 <label htmlFor="content">content: </label>
                 <input
                     type="text"
@@ -53,9 +69,10 @@ export default function PostForm({ form, setForm, handleSubmit }) {
                     onChange={e => setForm({ ...form, content: e.target.value })}
                     required
                 />
+                </div>
                 <button type="submit">Submit</button>
             </form>
-            
+
         </>
     )
 }

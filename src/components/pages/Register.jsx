05d1df27c1c84d4dcd9d107a7061574bd2ab8646
Billy Lu <billyfwlu@gmail.com>
2022-06-28 18:54:01 -0700

    👌 IMPROVE: added styling to login and register pages

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -21,7 +21,7 @@ export default function Register({ currentUser, setCurrentUser }) {
 				firstName,
 				lastName,
 				userName,
-				email, 
+				email,
 				password
 			}
 			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/register`, reqBody)
@@ -44,7 +44,7 @@ export default function Register({ currentUser, setCurrentUser }) {
 				}
 			}
 		}
- 	}
+	}
 
 	// conditionally render a navigate component
 	if (currentUser) {
@@ -52,59 +52,76 @@ export default function Register({ currentUser, setCurrentUser }) {
 	}
 
 	return (
-		<div>
-			<h1>Register for an account:</h1>
+		<div
+		className='flex justify-center mt-[10rem]'>
+			<div
+			className='flex flex-col h-[20rem] w-[25rem] border border-sm col-start-2 col-end-2 row-start-2 row-end-2'>
+				<h1>Register for an account:</h1>
 
-			<p>{msg}</p>
+				<p>{msg}</p>
 
-			<form onSubmit={handleSubmit}>
-				<label htmlFor='firstName'>First Name:</label>
-				<input 
-					type="text"
-					id="firstName"
-					placeholder='your first name...'
-					onChange={e => setFirstName(e.target.value)}
-					value={firstName}
-				/>
+				<form 
+				className='flex flex-col'
+				onSubmit={handleSubmit}>
+					<div>
+					<label htmlFor='firstName'>First Name:</label>
+					<input
+						type="text"
+						id="firstName"
+						placeholder='your first name...'
+						onChange={e => setFirstName(e.target.value)}
+						value={firstName}
+						required
+					/>
+					</div>
+					<div>
+					<label htmlFor='lastName'>Last Name:</label>
+					<input
+						type="text"
+						id="lastName"
+						placeholder='your last name...'
+						onChange={e => setLastName(e.target.value)}
+						value={lastName}
+						required
+					/>
+					</div>
+					<div>
+					<label htmlFor='userName'>User Name:</label>
+					<input
+						type="text"
+						id="userName"
+						placeholder='your user name...'
+						onChange={e => setUserName(e.target.value)}
+						value={userName}
+						required
+					/>
+					</div>
+					<div>
+					<label htmlFor='email'>Email:</label>
+					<input
+						type="email"
+						id="email"
+						placeholder='your email...'
+						onChange={e => setEmail(e.target.value)}
+						value={email}
+						required
+					/>
+					</div>
+					<div>
+					<label htmlFor='password'>Password:</label>
+					<input
+						type="password"
+						id="password"
+						placeholder='password...'
+						onChange={e => setPassword(e.target.value)}
+						value={password}
+						required
+					/>
+					</div>
 
-				<label htmlFor='lastName'>Last Name:</label>
-				<input 
-					type="text"
-					id="lastName"
-					placeholder='your last name...'
-					onChange={e => setLastName(e.target.value)}
-					value={lastName}
-				/>
-
-				<label htmlFor='userName'>User Name:</label>
-				<input 
-					type="text"
-					id="userName"
-					placeholder='your user name...'
-					onChange={e => setUserName(e.target.value)}
-					value={userName}
-				/>
-
-				<label htmlFor='email'>Email:</label>
-				<input 
-					type="email"
-					id="email"
-					placeholder='your email...'
-					onChange={e => setEmail(e.target.value)}
-					value={email}
-				/>
-
-				<label htmlFor='password'>Password:</label>
-				<input 
-					type="password"
-					id="password"
-					placeholder='password...'
-					onChange={e => setPassword(e.target.value)}
-					value={password}
-				/>
-
-				<button type="submit">Register</button>
-			</form>
+					<button type="submit">Register</button>
+				</form>
+			</div>
 		</div>
 	)
 }

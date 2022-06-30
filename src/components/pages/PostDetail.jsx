3f960f73c421a882084a53effc3b6169ce4fb34d
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-06-29 19:44:35 -0700

    reformatted most of PostDetail Page card to match the Feed cards, few more things to do

--- src/components/pages/PostDetail.jsx
+++ src/components/pages/PostDetail.jsx
@@ -72,33 +72,85 @@ export default function PostDetail({ currentUser }) {
             .catch(console.warn)
     }
     const renderDetail = (
-        <div className=
-            'border-slate-100 border-2 grid grid-cols-1 max-w-2xl place-content-center'>
+        <div
+            // className='border-slate-100 border rounded-lg grid grid-cols-1 max-w-2xl place-content-center shadow-md'
+            className='flex flex-col border my-[1rem] mx-[33rem] min-w-[50rem] w-[50rem] shadow-md rounded-lg'
+        >
+            <h2
+                className="text-start font-['Roboto'] pl-4 pt-1 font-black text-lg tracking-wide"
+            >
+                {post.poster.userName}
+            </h2>
+            <p
+                className="text-start font-['Roboto'] pl-5 pb-2 text-sm font-thin"
+            >
+                {post.dish.restaurant.name}
+            </p>
+            {/* <div 
+                className="m-2 justify-self-center w-md"
+            > */}
+            <img
+                className="w-[50rem] min-w-[50rem]"
+                src={form.img}
+                alt={post.dish.dishName}
+            />
+            {/* </div> */}
 
-            <div className="m-2 justify-self-center w-md">
-                <img src={form.img} alt={post.dish.dishName}
-                    className="w-[40rem] min-w-[40rem]" />
+            <div 
+                className="grid grid-cols-2"
+            >
+
+                {/* <h2
+                    className="text-lg font-['Roboto'] font-bold"
+                >
+                    {post.dish.dishName}
+                </h2> */}
+                <p
+                    className="text-start font-['Roboto'] pl-4 pt-2 pb-2 text-md font-semibold "
+                >
+                    {post.dish.dishName}
+                </p>
+                <h2
+                    className="text-md font-['Roboto'] font-semibold text-end pt-2 pb-2 pr-9"
+                >
+                    Rating: {post.rating}
+                </h2>
             </div>
-
-                <div className="grid grid-cols-2">
-                    <h3 className="text-lg font-sans font-bold justify-self-start">{post.poster.userName}</h3>
-                    <h2 className="text-lg font-sans font-bold justify-self-center">#Rate: {post.rating}</h2>
-                </div>
-            <div className="justify-self-start">
-                <h2 className="text-lg font-sans font-bold">#Dish : {post.dish.dishName}</h2>
-                <h2 className="text-lg font-sans font-bold">#Restaurant: {post.dish.restaurant.name}</h2>
-                <p className="text-base font-sans text-center break-all">{post.content}</p>
+            <div
+                className="justify-self-start px-3"
+            >
+                {/* <h2
+                    className="text-lg font-['Roboto'] font-bold"
+                >
+                    {post.dish.dishName}
+                </h2>
+                <h2
+                    className="text-lg font-['Roboto'] font-bold"
+                >
+                    {post.dish.restaurant.name}
+                </h2> */}
+                <p
+                    className="text-base font-['Roboto'] text-start pb-[1rem] px-3"
+                >
+                    {post.content}
+                </p>
 
             </div>
-            <div className="grid grid-cols-2 text-blue-600">
+            <div
+                className="grid pb-2 px-4 grid-cols-2"
+            >
                 {post.poster._id === currentUser.id ? <button
-                    className="border-slate-300 border-2 w-20 justify-start"
-                    onClick={() => setShowForm(!showForm)}>Edit</button>
-                    : ''}
+                    className="rounded-md w-20 bg-blue-600 text-white justify-start font-semibold font-['Roboto']"
+                    onClick={() => setShowForm(!showForm)}
+                >
+                    Edit
+                </button> : ''}
                 {post.poster._id === currentUser.id ? <button
-                    className="border-slate-300 border-2 w-20 text-red-600 justify-self-end"
-                    onClick={() => handleDelete()}>Delete</button>
-                    : ''}
+                    className="rounded-md w-20 text-white bg-red-600 justify-self-end font-semibold font-['Roboto']"
+                    onClick={() => handleDelete()}
+                >
+                    Delete
+                </button> : ''}
             </div>
 
 
